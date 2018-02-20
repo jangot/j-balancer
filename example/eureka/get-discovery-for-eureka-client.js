d449ed1d17b934f459478e67afdd4254a79a7d79
ppilin <ppulin@alfabank.ru>
2018-02-20 18:24:34 +0300

    clone eureka hosts

--- example/eureka/get-discovery-for-eureka-client.js
+++ example/eureka/get-discovery-for-eureka-client.js
@@ -2,7 +2,7 @@
 module.exports = function (hosts) {
     return {
         getHosts: () => {
-            return Promise.resolve(hosts)
+            return Promise.resolve([...hosts])
         }
     };
 };

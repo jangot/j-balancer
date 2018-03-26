91c25313552ea21e41a027b5287e58b83e92bb74
ppilin <ppulin@alfabank.ru>
2018-03-26 18:43:01 +0300

    15

--- test/client.test.js
+++ test/client.test.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
 const Client = require('../src/client');
-const DiscoveryError = require('../src/discoveryError');
 
 const mock = new MockAdapter(axios);
 

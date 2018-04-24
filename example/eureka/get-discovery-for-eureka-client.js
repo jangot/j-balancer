2a6477fb49952504edef4f4193d3cd260d29cb32
ppilin <ppulin@alfabank.ru>
2018-04-24 23:32:41 +0300

    shake eureka discovery hosts

--- example/eureka/get-discovery-for-eureka-client.js
+++ example/eureka/get-discovery-for-eureka-client.js
@@ -2,7 +2,10 @@
 module.exports = function (hosts) {
     return {
         getHosts: (name) => {
-            return Promise.resolve([...hosts])
+            const first = hosts.shift();
+            hosts.push(first);
+
+            return Promise.resolve([...hosts]);
         }
     };
 };
